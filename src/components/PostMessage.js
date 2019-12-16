8512564ddd4748ee1a688f4d10ad419859d42ad9
Larry Cohen <cohlar@gmail.com>
2019-12-16 16:54:43 +0200

    added error message for msg > 140 chars

--- src/components/PostMessage.js
+++ src/components/PostMessage.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-export default function PostMessage(props) {
-    return (
-        <div className='post-msg-container'>
-            <textarea placeholder='What you have in mind...' className='post-msg-input'></textarea>
-            <button className='post-msg-btn'>Post</button>
-        </div>
-    );
+export default class PostMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+        }
+    }
+
+    render() {
+        const { message } = this.state;
+        const isTooLong = (message.length > 140);
+        return (
+            <div className='post-msg-container'>
+
+                <textarea
+                    placeholder='What you have in mind...'
+                    className='post-msg-input'
+                    onChange={(event) => this.setState({ message: event.target.value })}
+                >
+                </textarea>
+
+                <div className='btn-container'>
+                    <span className='error-placeholder'>
+                        {isTooLong &&
+                            <span className='error-msg'>
+                                The message can't contain more than 140 chars.
+                            </span>
+                        }
+                    </span>
+                    <button
+                        className='post-msg-btn'
+                        disabled={isTooLong}
+                    >
+                        Post
+                    </button>
+                </div>
+
+            </div>
+        );
+    }
 }
